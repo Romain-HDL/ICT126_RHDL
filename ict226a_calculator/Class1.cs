43c3a10118fe693c3385c82e34554eb9cf9003f4
Romain.HUMBERT-DROZ <Romain.HUMBERT-DROZ@SC-C315-PC06>
2022-09-02 11:10:20 +0200

    Add project files.

---
+++ ict226a_calculator/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator2
+{
+    public class MathsBasicOperation
+    {
+        /// <summary>
+        /// This method is designed to return the addition of 2 numbers
+        /// </summary>
+        /// <param name="op1">First number choosed by the user</param>
+        /// <param name="op2">Second number choosed by the user</param>
+        /// <returns></returns>
+        public int Add(int op1, int op2)
+        {
+            return op1 + op2;
+        }
+
+        /// <summary>
+        /// This method is designed to return the substraction of 2 numbers
+        /// </summary>
+        /// <param name="op1">First number choosed by the user</param>
+        /// <param name="op2">Second number choosed by the user</param>
+        /// <returns></returns>
+        public int Substract(int op1, int op2)
+        {
+            return op1 - op2;
+        }
+
+        /// <summary>
+        /// This method is designed to return the multiplication of 2 numbers
+        /// </summary>
+        /// <param name="op1">First number choosed by the user</param>
+        /// <param name="op2">Second number choosed by the user</param>
+        /// <returns></returns>
+        public int Multiply(int op1, int op2)
+        {
+            return op1 * op2;
+        }
+
+        /// <summary>
+        /// This method is designed to return the division of 2 numbers
+        /// </summary>
+        /// <param name="op1">First number choosed by the user</param>
+        /// <param name="op2">Second number choosed by the user</param>
+        /// <returns></returns>
+        public int Divide(int op1, int op2)
+        {
+            return op1 / op2;
+        }
+    }
+}
---
+++ ict226a_calculator/Program.cs
@@ -0,0 +1,111 @@
+﻿using System.ComponentModel;
+using Calculator2;
+
+namespace Calculator
+{
+    class Program
+    {
+        #region var section
+        static int result;
+        static int op1;
+        static int op2;
+        static char oper;
+        static char[] operators = {'+','-','*','/'};
+        #endregion
+
+        /// <summary>
+        /// This method is the entry point of the application
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // checks if the operator the user entered is in the list, if not, it will a new time ask the operator
+            do
+            {
+                oper = AskUserForCharInput("Rentrez votre opérateur :");
+            } while(!operators.Contains(oper));
+
+            op1 = AskUserForInteger("Rentrez votre premier numéro :");
+            op2 = AskUserForInteger("Rentrez votre second numéro :");
+
+            int result = PerformCalculation(op1, op2, oper);
+
+            DisplayResult(op1, oper, op2, result);
+        }
+
+        /// <summary>
+        /// This method is designed to ask to the user a math operator
+        /// </summary>
+        /// <param name="question">A question who will be shown to the user</param>
+        /// <returns></returns>
+        static char AskUserForCharInput(string question)
+        {
+            DisplayMessage(question);
+
+            char[] calcOperator = Console.ReadLine().ToCharArray();
+
+            return calcOperator[0];
+        }
+
+        /// <summary>
+        /// This method is designed to ask to the user a number
+        /// </summary>
+        /// <param name="question">A question who will be shown to the user</param>
+        /// <returns></returns>
+        static int AskUserForInteger(string question)
+        {
+            DisplayMessage(question);
+
+            int number = Int32.Parse(Console.ReadLine());
+
+            return number;
+        }
+
+        /// <summary>
+        /// This method is designed to show to the user the result
+        /// </summary>
+        /// <param name="op1">First number choosed by the user</param>
+        /// <param name="oper">Math operator choosed by the user </param>
+        /// <param name="op2">Second number choosed by the user</param>
+        /// <param name="result">Result (Add/Substract/Multiply/Divide)</param>
+        static void DisplayResult(int op1, char oper, int op2, int result)
+        {
+            DisplayMessage($"{op1} {oper} {op2} = {result}");
+        }
+
+        /// <summary>
+        /// This method is designed to show to the user any message
+        /// </summary>
+        /// <param name="message">Message to be displayed</param>
+        static void DisplayMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        static int PerformCalculation(int op1, int op2, char oper)
+        {
+            MathsBasicOperation mathOperation = new MathsBasicOperation();
+
+            switch (oper)
+            {
+                case '+':
+                    result = mathOperation.Add(op1, op2);
+                    break;
+
+                case '-':
+                    result = mathOperation.Substract(op1, op2);
+                    break;
+
+                case '*':
+                    result = mathOperation.Multiply(op1, op2);
+                    break;
+
+                case '/':
+                    result = mathOperation.Divide(op1, op2);
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
